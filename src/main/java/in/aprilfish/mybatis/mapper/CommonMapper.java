a8a8b92071b8d9effe0bee2b790d4cf19f46383e
zhaohq <hongqing.zhao@transsnet.com>
2021-02-03 10:38:56 +0800

    add CommonMapper

---
+++ src/main/java/in/aprilfish/mybatis/mapper/CommonMapper.java
@@ -0,0 +1,151 @@
+package in.aprilfish.mybatis.mapper;
+
+import in.aprilfish.mybatis.provider.*;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+/**
+ * 通用Mapper，实现基本功能
+ *
+ * @author meilin.huang
+ * @param <I>  主键类型
+ * @param <E>  实体类型
+ */
+public interface CommonMapper<I, E> {
+
+    /**
+     * 插入新对象,并返回主键id值(id通过实体获取)
+     *
+     * @param entity 实体对象
+     * @return  影响条数
+     */
+    @InsertProvider(type = DynamicSqlProvider.class, method = "insert")
+    @Options(useGeneratedKeys = true, keyColumn = TableInfo.DEFAULT_PRIMARY_KEY)
+    int insert(E entity);
+
+    /**
+     * 插入新对象（只设置非空字段）,并返回主键id值(id通过实体获取)
+     *
+     * @param entity 实体对象
+     * @return  影响条数
+     */
+    @InsertProvider(type = DynamicSqlProvider.class, method = "insertSelective")
+    @Options(useGeneratedKeys = true, keyColumn = TableInfo.DEFAULT_PRIMARY_KEY)
+    int insertSelective(E entity);
+
+    /**
+     * 批量插入实体
+     *
+     * @param entities  实体列表
+     * @return          影响条数
+     */
+    @InsertProvider(type = DynamicSqlProvider.class, method = "batchInsert")
+    int batchInsert(@Param("entities") List<E> entities);
+
+    /**
+     * 根据主键id更新实体，若实体field为null，则对应数据库的字段也更新为null
+     *
+     * @param entity  实体对象
+     * @return         影响条数
+     */
+    @UpdateProvider(type = DynamicSqlProvider.class, method = "updateByPrimaryKey")
+    int updateByPrimaryKey(E entity);
+
+    /**
+     * 根据主键id更新实体，若实体field为null，则对应数据库的字段不更新
+     *
+     * @param entity  实体对象
+     * @return        影响条数
+     */
+    @UpdateProvider(type = DynamicSqlProvider.class, method = "updateByPrimaryKeySelective")
+    int updateByPrimaryKeySelective(E entity);
+
+    /**
+     * 根据主键id删除
+     *
+     * @param id  id
+     * @return  影响条数
+     */
+    @DeleteProvider(type = DynamicSqlProvider.class, method = "deleteByPrimaryKey")
+    int deleteByPrimaryKey(I id);
+
+    /**
+     * 伪删除，即将is_deleted字段更新为1
+     *
+     * @param id id
+     * @return  影响条数
+     */
+    @UpdateProvider(type = DynamicSqlProvider.class, method = "fakeDeleteByPrimaryKey")
+    int fakeDeleteByPrimaryKey(I id);
+
+    /**
+     * 根据实体条件删除
+     *
+     * @param criteria  实体
+     * @return  影响条数
+     */
+    @DeleteProvider(type = DynamicSqlProvider.class, method = "deleteByCriteria")
+    int deleteByCriteria(E criteria);
+
+    /**
+     * 根据id查询实体
+     *
+     * @param id  id
+     * @return    实体
+     */
+    @SelectProvider(type = DynamicSqlProvider.class, method = "selectByPrimaryKey")
+    E selectByPrimaryKey(I id);
+
+    /**
+     * 查询所有实体
+     *
+     * @param orderBy  排序
+     * @return   实体list
+     */
+    @SelectProvider(type = DynamicSqlProvider.class, method = "selectAll")
+    List<E> selectAll(String orderBy);
+
+    /**
+     * 根据id列表查询实体列表
+     * @param ids  id列表
+     * @return  list
+     */
+    @SelectProvider(type = DynamicSqlProvider.class, method = "selectByPrimaryKeyIn")
+    List<E> selectByPrimaryKeyIn(@Param("ids") List<I> ids);
+
+    /**
+     * 根据实体条件查询符合条件的实体list
+     * @param criteria  条件实体
+     * @return          list
+     */
+    @SelectProvider(type = DynamicSqlProvider.class, method = "selectByCriteria")
+    List<E> selectByCriteria(E criteria);
+
+    /**
+     * 根据条件查询单个数据
+     *
+     * @param criteria  实体条件
+     * @return          实体对象
+     */
+    @SelectProvider(type = DynamicSqlProvider.class, method = "selectOneByCriteria")
+    E selectOneByCriteria(E criteria);
+
+    /**
+     * 返回实体总数
+     *
+     * @return  总数
+     */
+    @SelectProvider(type = DynamicSqlProvider.class, method = "count")
+    long count();
+
+    /**
+     * 根据条件查询符合条件的实体总数
+     *
+     * @param criteria  实体条件
+     * @return    数量
+     */
+    @SelectProvider(type = DynamicSqlProvider.class, method = "countByCriteria")
+    long countByCriteria(E criteria);
+
+}
---
+++ src/main/java/in/aprilfish/mybatis/provider/DynamicSqlProvider.java
@@ -0,0 +1,223 @@
+package in.aprilfish.mybatis.provider;
+
+import in.aprilfish.mybatis.util.PlaceholderResolver;
+import in.aprilfish.mybatis.util.ReflectionUtils;
+import in.aprilfish.mybatis.util.StringUtils;
+import org.apache.ibatis.builder.annotation.ProviderContext;
+import org.apache.ibatis.jdbc.SQL;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+public class DynamicSqlProvider {
+    /**
+     * key -> mapper class   value -> tableInfo
+     */
+    private static Map<Class<?>, TableInfo> tableCache = new ConcurrentHashMap<>(128);
+
+    /**
+     * 获取表信息结构
+     *
+     * @param context provider context
+     * @return 表基本信息
+     */
+    private TableInfo tableInfo(ProviderContext context) {
+        // 如果不存在则创建
+        return tableCache.computeIfAbsent(context.getMapperType(), TableInfo::of);
+    }
+
+    public String insert(ProviderContext context) {
+        TableInfo table = tableInfo(context);
+
+        return new SQL()
+                .INSERT_INTO(table.getTableName())
+                .INTO_COLUMNS(table.getColumns())
+                .INTO_VALUES(Stream.of(table.getFields()).map(TableInfo::bindParameter).toArray(String[]::new))
+                .toString();
+    }
+
+    public String insertSelective(Object entity, ProviderContext context) {
+        TableInfo table = tableInfo(context);
+
+        Field[] notNullFields = Stream.of(table.getFields())
+                .filter(field -> ReflectionUtils.getFieldValue(field, entity) != null && !table.getPrimaryKeyColumn().equals(TableInfo.columnName(field)))
+                .toArray(Field[]::new);
+
+        return new SQL()
+                .INSERT_INTO(table.getTableName())
+                .INTO_COLUMNS(TableInfo.columns(notNullFields))
+                .INTO_VALUES(Stream.of(notNullFields).map(TableInfo::bindParameter).toArray(String[]::new))
+                .toString();
+    }
+
+    public String batchInsert(Map<String, Object> param, ProviderContext context) {
+        TableInfo table = tableInfo(context);
+        @SuppressWarnings("unchecked")
+        int size = ((List<Object>) param.get("entities")).size();
+        // 构造 ( #{entities[1-->数组索引].fieldName}, #{entities[1].fieldName2})
+        String value = "(" + String.join(",", Stream.of(table.getFields())
+                .map(field -> "#{entities[${index}]." + field.getName() + "}").toArray(String[]::new)) + ")";
+        String[] values = new String[size];
+        Map<String, Object> fillIndex = new HashMap<>(2);
+        for (int i = 0; i < size; i++) {
+            fillIndex.put("index", i);
+            values[i] = PlaceholderResolver.getDefaultResolver().resolveByMap(value, fillIndex);
+        }
+
+        SQL sql = new SQL()
+                .INSERT_INTO(table.getTableName())
+                .INTO_COLUMNS(table.getColumns());
+        return sql.toString() + " VALUES " + String.join(",", values);
+    }
+
+    public String selectByPrimaryKey(ProviderContext context) {
+        TableInfo table = tableInfo(context);
+
+        return new SQL()
+                .SELECT(table.getSelectColumns())
+                .FROM(table.getTableName())
+                .WHERE(table.getPrimaryKeyWhere())
+                .toString();
+    }
+
+    public String selectByPrimaryKeyIn(Map<String, Object> params, ProviderContext context) {
+        @SuppressWarnings("unchecked")
+        List<Object> ids = (List<Object>) params.get("ids");
+        TableInfo table = tableInfo(context);
+        return new SQL()
+                .SELECT(table.getSelectColumns())
+                .FROM(table.getTableName())
+                .WHERE(table.getPrimaryKeyColumn()
+                        + " IN (" + String.join(",", ids.stream().map(String::valueOf).toArray(String[]::new)) + ")")
+                .toString();
+    }
+
+    /**
+     * sql
+     *
+     * @param orderBy 排序字段
+     * @param context context
+     * @return sql
+     */
+    public String selectAll(String orderBy, ProviderContext context) {
+        TableInfo table = tableInfo(context);
+        SQL sql = new SQL()
+                .SELECT(table.getSelectColumns())
+                .FROM(table.getTableName());
+        if (StringUtils.isEmpty(orderBy)) {
+            orderBy = table.getPrimaryKeyColumn() + " DESC";
+        }
+        return sql.ORDER_BY(orderBy).toString();
+    }
+
+    /**
+     * sql
+     *
+     * @param criteria entity 条件
+     * @param context  context
+     * @return sql
+     */
+    public String selectByCriteria(Object criteria, ProviderContext context) {
+        TableInfo table = tableInfo(context);
+        return new SQL()
+                .SELECT(table.getSelectColumns())
+                .FROM(table.getTableName())
+                .WHERE(Stream.of(table.getFields())
+                        .filter(field -> ReflectionUtils.getFieldValue(field, criteria) != null)
+                        .map(TableInfo::assignParameter)
+                        .toArray(String[]::new)).ORDER_BY(table.getPrimaryKeyColumn() + " DESC").toString();
+    }
+
+    /**
+     * sql
+     *
+     * @param criteria entity 条件
+     * @param context  context
+     * @return sql
+     */
+    public String count(Object criteria, ProviderContext context) {
+        TableInfo table = tableInfo(context);
+        return new SQL()
+                .SELECT("COUNT(*)")
+                .FROM(table.getTableName())
+                .toString();
+    }
+
+    /**
+     * sql
+     *
+     * @param criteria entity 条件
+     * @param context  context
+     * @return sql
+     */
+    public String countByCriteria(Object criteria, ProviderContext context) {
+        TableInfo table = tableInfo(context);
+        return new SQL()
+                .SELECT("COUNT(*)")
+                .FROM(table.getTableName())
+                .WHERE(Stream.of(table.getFields())
+                        .filter(field -> ReflectionUtils.getFieldValue(field, criteria) != null)
+                        .map(TableInfo::assignParameter).toArray(String[]::new))
+                .toString();
+    }
+
+    public String update(ProviderContext context) {
+        TableInfo table = tableInfo(context);
+
+        return new SQL()
+                .UPDATE(table.getTableName())
+                .SET(Stream.of(table.getFields())
+                        .filter(field -> !table.getPrimaryKeyColumn().equals(TableInfo.columnName(field)))
+                        .map(TableInfo::assignParameter).toArray(String[]::new))
+                .WHERE(table.getPrimaryKeyWhere())
+                .toString();
+    }
+
+    public String updateSelective(Object entity, ProviderContext context) {
+        TableInfo table = tableInfo(context);
+
+        return new SQL()
+                .UPDATE(table.getTableName())
+                .SET(Stream.of(table.getFields())
+                        .filter(field -> ReflectionUtils.getFieldValue(field, entity) != null && !table.getPrimaryKeyColumn().equals(TableInfo.columnName(field)))
+                        .map(TableInfo::assignParameter).toArray(String[]::new))
+                .WHERE(table.getPrimaryKeyWhere())
+                .toString();
+    }
+
+    public String fakeDelete(ProviderContext context) {
+        TableInfo table = tableInfo(context);
+
+        return new SQL()
+                .UPDATE(table.getTableName())
+                .SET("is_deleted = 1")
+                .WHERE(table.getPrimaryKeyColumn() + " = #{id}")
+                .toString();
+    }
+
+    public String delete(ProviderContext context) {
+        TableInfo table = tableInfo(context);
+
+        return new SQL()
+                .DELETE_FROM(table.getTableName())
+                .WHERE(table.getPrimaryKeyColumn() + " = #{id}")
+                .toString();
+    }
+
+    public String deleteByCriteria(Object criteria, ProviderContext context) {
+        TableInfo table = tableInfo(context);
+
+        return new SQL()
+                .DELETE_FROM(table.getTableName())
+                .WHERE(Stream.of(table.getFields())
+                        .filter(field -> ReflectionUtils.getFieldValue(field, criteria) != null)
+                        .map(TableInfo::assignParameter)
+                        .toArray(String[]::new))
+                .toString();
+    }
+
+}
